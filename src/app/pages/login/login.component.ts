da6e295d01a6557ab3ec350f7f604e3bae4a4566
Shweta Patil <shwetarpatil024@gmail.com>
2024-08-01 01:01:29 -0700

    Added a reactive form for login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  userloginForm = new FormGroup({
+    userName: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(5)])
+  })
 
+  onSubmit() {
+    console.log(this.userloginForm.value)
+  }
 }
