4b422d9f614193fe667fea59be04e0342c3aadd9
Shweta Patil <shwetarpatil024@gmail.com>
2024-08-22 22:21:12 +0530

    1. Updated songs 2. Added padding to the cards 3. Handled song paly and pause of each song

--- src/app/components/song-card/song-card.component.ts
+++ src/app/components/song-card/song-card.component.ts
@@ -5,7 +5,8 @@ import { MatIconModule } from '@angular/material/icon';
 type Song = {
   title: string,
   description: string,
-  image: string 
+  image: string,
+  audioUrl: any;
 }
 
 @Component({
@@ -21,6 +22,7 @@ export class SongCardComponent implements OnInit {
   filteredSongs: Song[] = [];
   audio: HTMLAudioElement | undefined;
   isPlaying: boolean = false;
+  currentPlayingIndex: number | null = null;
   constructor() { }
   
   ngOnInit() {
@@ -30,124 +32,148 @@ export class SongCardComponent implements OnInit {
   getSongList() {
     this.allSongs = [
       {
-        "title": "Death Bed",
-        'description': 'Description of Song 1',
-        'image': "../../../assets/Music-Icon.png"
-      },
-      {
-        'title': 'Morning Mr Magpie',
+        'title': 'Alien ost',
         'description': 'Description of Song 2',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2022/01/08/audio_8a76d1ca5b.mp3"
       },
       {
-        'title': 'Little by Little',
+        'title': 'Sky',
         'description': 'Description of Song 1',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2022/01/08/audio_9bb82558f6.mp3"
       },
       {
-        'title': 'Feral',
-        'description': 'Description of Song 2',
-        'image': "../../../assets/Music-Icon.png"
-      },
-      {
-        'title': 'Lotus Flower',
+        "title": "Get Some Rest",
         'description': 'Description of Song 1',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/08/30/audio_0bc5137ad4.mp3"
       },
       {
-        'title': 'Lotus Flower',
+        'title': 'Be With You',
         'description': 'Description of Song 2',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/06/15/audio_b8328c7a49.mp3"
       },
       {
-        'title': 'Give Up the Ghost',
+        'title': 'Minimalistic Light Ambient',
         'description': 'Description of Song 1',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2024/04/10/audio_a002e4c68e.mp3"
       },
       {
-        'title': 'Separator',
+        'title': 'happy edm',
         'description': 'Description of Song 2',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/12/09/audio_445787f109.mp3"
       },
       {
-        'title': 'Airbag',
+        'title': 'Minimalistic Inspiration Design Ambient',
         'description': 'Description of Song 1',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2024/04/10/audio_700ba63d30.mp3"
       },
       {
-        'title': 'Paranoid Android',
+        'title': 'Minimalistic Design Ambient Background',
         'description': 'Description of Song 2',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/10/05/audio_8544d22db8.mp3"
+      },
+      {
+        'title': 'Hypnotic Hip Hop Beat',
+        'description': 'Description of Song 1',
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/01/05/audio_297daed87c.mp3"
+      },
+      {
+        'title': 'Cupid / Stylish Upbeat RnB Beat',
+        'description': 'Description of Song 2',
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/04/30/audio_af76a50d34.mp3"
       },
       {
         'title': 'Subterranean Homesick Alien',
         'description': 'Description of Song 1',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/10/05/audio_8544d22db8.mp3"
       },
       {
-        'title': 'Exit Music (For a Film)',
+        'title': 'Phonk Less',
         'description': 'Description of Song 2',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/09/26/audio_0275760908.mp3"
       },
       {
-        'title': 'Let Down',
+        'title': 'What happen to me',
         'description': 'Description of Song 1',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2024/04/27/audio_5e68a7fb68.mp3"
       },
       {
-        'title': 'Karma Police',
+        'title': 'Whiplash - Audio Tonic',
         'description': 'Description of Song 2',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/12/21/audio_60b53d002a.mp3"
       },
       {
-        'title': 'Fitter Happier',
+        'title': 'HIPHOP Simple beat 2',
         'description': 'Description of Song 1',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/08/04/audio_3aea8c32bf.mp3"
       },
       {
-        'title': 'Electioneering',
+        'title': 'Crash',
         'description': 'Description of Song 2',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/12/09/audio_445787f109.mp3"
       },
       {
-        'title': 'Climbing Up the Wall',
+        'title': 'Happy edm',
         'description': 'Description of Song 1',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/12/09/audio_445787f109.mp3"
       },
       {
-        'title': 'No Surprises',
+        'title': 'Hipnotic Hip Hop Beat',
         'description': 'Description of Song 2',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2024/01/09/audio_7355016257.mp3"
       },
       {
-        'title': 'Lucky',
+        'title': 'Quick Spit',
         'description': 'Description of Song 1',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2024/01/09/audio_7355016257.mp3"
       },
       {
-        'title': 'Mysterons',
+        'title': 'Deep House Arcade',
         'description': 'Description of Song 2',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/06/25/audio_ded864e440.mp3"
       },
       {
-        'title': 'Sour Times',
+        'title': 'Glitch',
         'description': 'Description of Song 1',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2022/12/17/audio_e02ae20435.mp3"
       },
       {
         'title': 'Strangers',
-        'description': 'Description of Song 2',
-        'image': "../../../assets/Music-Icon.png"
+        'description': 'Cinematic Rythmline',
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/06/02/audio_320a2e0f57.mp3"
       },
       {
-        'title': 'It Could Be Sweet',
+        'title': 'Ocean Breeze beat by JTWAYNE',
         'description': 'Description of Song 1',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2024/05/29/audio_f3a1d24f19.mp3"
       },
       {
-        'title': 'Wandering Star',
+        'title': 'Perfection',
         'description': 'Description of Song 2',
-        'image': "../../../assets/Music-Icon.png"
+        'image': "../../../assets/Music-Icon.png",
+        'audioUrl': "https://cdn.pixabay.com/audio/2023/06/15/audio_b8328c7a49.mp3"
       }
     ]
   }
@@ -166,15 +192,20 @@ export class SongCardComponent implements OnInit {
       }
     }
   }
-  playSong() {
-    this.audio = new Audio("https://cdn.pixabay.com/audio/2023/10/05/audio_8544d22db8.mp3");
+  playSong(song: Song, index: number) {
+    this.audio = new Audio(song.audioUrl);
     this.audio.play();
-    this.isPlaying = true;
+    if (this.audio && this.currentPlayingIndex !== null && this.currentPlayingIndex !== index) {
+      this.audio.pause();
+    }
+    this.currentPlayingIndex = index;    
   }
 
   pauseSong() {
-    this.audio?.pause();
-    this.isPlaying = false;
+    if (this.audio) {
+      this.audio.pause();
+    }
+    this.currentPlayingIndex = null;
   }
 
 }
